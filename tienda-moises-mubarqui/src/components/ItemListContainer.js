ad1be179794b9ce0ea6e35cf6e3a9e703ec0fe98
Nemerd <xtremestripedtiger@gmail.com>
2022-08-23 20:16:39 -0300

    ItemListContent added with styles

---
+++ tienda-moises-mubarqui/src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import './ItemListContainer.css';
+
+function ItemListContainer() {
+    return(
+        <div className='ItemListContainer'>
+            Hello world
+        </div>
+    )
+}
+
+export default ItemListContainer;
