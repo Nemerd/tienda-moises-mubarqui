6fe60bde97ed5ca1e1efc20a233dcb08a045c9a3
Nemerd <xtremestripedtiger@gmail.com>
2022-09-07 20:29:39 -0300

    Cleanup

--- tienda-moises-mubarqui/src/App.js
+++ tienda-moises-mubarqui/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import ItemList from './components/ItemList';
-import mockItems from './mock/Items-mockup.json';
-import ItemDetailContainer from './components/ItemDetailContainer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <ItemList items={mockItems} /> */}
-      <ItemDetailContainer id={1}/>
+      
     </div>
   );
 }
